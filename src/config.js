f6a7c1d05a069c46f241e2939fd48c19b9d221b9
bello993666-maker <bello993666@gmail.com>
2026-05-31 17:10:23 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
 	//网页元数据
 	metaData: {
 		title: 'Leleo的个人主页🎉',
-		description: '欢迎来到Leleo的奇妙世界！',
+		description: '欢迎来到bello的奇妙世界！',
 		keywords: 'Leleo,leleo,个人主页,个人网站',
 		icon: '/favicon.ico'   //网页图标，支持外链
 	},
